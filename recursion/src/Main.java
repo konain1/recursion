d6c0f68d924d0d9c2bb0db014126fe0acc5b540a
Konain1 <kaunainkasab@gmail.com>
2023-11-29 14:32:33 +0530

    printSpell

--- recursion/src/Main.java
+++ recursion/src/Main.java
@@ -1,29 +1,42 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
 //        Scanner scan = new Scanner(System.in);
 //
 //        int n = scan.nextInt();
-        int n = 5;
+        int n = 432;
         int m = 1;
+        String [] arr ={ "zero","one","two","three","four","five","six","seven","eight","nine"};
+
 //        Reversecounting(n);
 //        counting(m);
 
 //        int resutlt =fact(n);
 //        System.out.println(resutlt);
 
-        int ans = fibo(n);
+//        int ans = fibo(n);
+
+
+//        System.out.println(ans);
+
+        printSpell(n, arr);
+    }
+    public static void printSpell(int n, String[] str){
+        if(n == 0){
+            return;
+        }
+
+         printSpell(n/10,str);
+        System.out.println(str[n%10]);
     }
 public static int fibo(int n){
         if(n == 0|| n ==1){
             return n;
         }
 
-        int res = fibo(n-1) + fibo(n-2);
-        return res;
+        int res =  fibo(n-1) + fibo(n-2);
 
+    return res;
 
 }
     public static int fact(int n){
